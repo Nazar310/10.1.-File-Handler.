7e7c9419491fb8e8cf2a72f16df9f85848dd12be
Nazar <nazar31032009@gmail.com>
2025-04-07 17:02:55 +0300

    fix/ FileHandler

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -5,26 +5,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHandler {
-
     private final static String BASE_PATH = "files/";
 
-    public String writeFile(String fileContent) {
-        try(FileWriter fw = fileName + ".txt") {
-            fw(fileContent);
+    // запис
+    public String writeFile(String fileName, String fileContent) {
+        try (FileWriter fw = new FileWriter(BASE_PATH + fileName + ".txt")) {
+            fw.write(fileContent);
             return "Success.";
         } catch (Exception e) {
-            return
+            return e.getMessage();
         }
     }
 
+    // читання
     public String readFile(String path) {
-        try (FileReader reader = new FileReader()) {
-            int sym;
-            new StringBuilder();
-            while ((sym = reader.read()) != -1) {
-                stringBuilder.append((char) sym);
+        try (FileReader reader = new FileReader(path)) {
+            int symbol;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((symbol = reader.read()) != -1) {
+                stringBuilder.append((char) symbol);
             }
-            return
+
+            return stringBuilder.toString();
         } catch (IOException ex) {
             return ex.getMessage();
         }
